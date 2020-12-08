5af2bd332c65ca1f03ad897751c614c9bb1b2fb7
John Shenk <john_shenk@hotmail.com>
2020-12-08 14:03:52 -0600

    add backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,13 @@ resource "aws_s3_bucket" "storyrama" {
   bucket = "storyrama"
   acl = "private"
 }
+
+# backend
+terraform {
+  backend "s3" {
+    bucket = "remotebackend"
+    key    = "storyrama/terraform.tfstate"
+    region = "us-west-1"
+    profile = "jds"
+  }
+}
