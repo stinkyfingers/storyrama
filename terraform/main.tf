bb058390957a860402252d769fd8e9c3f6c252bb
John Shenk <john_shenk@hotmail.com>
2020-12-08 14:05:52 -0600

    comment s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-west-1"
 }
 
-resource "aws_s3_bucket" "storyrama" {
-  bucket = "storyrama"
-  acl = "private"
-}
+# resource "aws_s3_bucket" "storyrama" {
+#   bucket = "storyrama"
+#   acl = "private"
+# }
 
 # backend
 terraform {
